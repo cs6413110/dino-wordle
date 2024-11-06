93b8455cd5f0c96d0bfbe319eedd33368df1e6bc
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-11-06 11:11:15 -0700

    buff

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 document.documentElement.innerHTML += `
 <script src='https://cs6413110.github.io/dino-wordle/config.js'></script>
+<div id='top'>
+  <h1>Dino of the Day</h1>
+</div>
 <div id='centered'>
   <div id='name'></div>
   <img src=''></img>
-<div>
+</div>
 <div id='guess'>
   <input id='' />
   <button>Guess</button>
@@ -14,7 +17,20 @@ document.documentElement.innerHTML += `
     padding: 0;
     width: 100%;
     height: 100%;
+    font-size: 4vh;
     overflow: hidden;
+    font-family: Georgia;
+  }
+  #top {
+    text-align: center;
+    position: absolute;
+    width: 100%;
+    height: 10%;
+    background-color: white;
+    border-bottom: 1px solid #C3C3C3;
+  }
+  #top h1 {
+    margin: 0;
   }
   #centered {
     width: 50%;
@@ -24,8 +40,8 @@ document.documentElement.innerHTML += `
     margin-right: 25%;
   }
   #guess {
-    width: 0;
     position: absolute;
+    border-top: 1px solid #C3C3C3;
     width: 100%;
     height: 10%;
     margin: 0;
@@ -36,10 +52,18 @@ document.documentElement.innerHTML += `
     height: 100%;
     border: 0;
     outline: none;
+    font-family: Georgia;
+    font-size: 5vh;
   }
   button {
     width: 10%;
     height: 100%;
+    border: 0;
+    font-family: Georgia;
+    font-size: 4vh;
+    margin: 0;
+    padding: 0;
+    background-color: lightblue;
   }
 </style>`;
 const answer = ;
