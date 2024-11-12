d54c3b0c574842323c8b47a6195f31df48a68b3f
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-11-12 13:12:30 -0700

    fix

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const dinos = [
   {name: 'tyrannosaurus', image: 'https://cdn.mos.cms.futurecdn.net/aaYjPnEgnqGYFndmnpShWZ-1200-80.jpg'},
   {name: 'therizinosaur', image: 'https://i.redd.it/8y9u1bn3q3g91.jpg'},
   {name: 'baryonyx', image: 'http://images.dinosaurpictures.org/Baryonyx-Andrey-Atuchin_3b3e.jpg'},
-  {name: 'bread when chicken nugget', image: 'https://i.redd.it/s4crv4cy5yx91.jpg'},
+  {name: 'carnotaurus', image: 'https://i.redd.it/s4crv4cy5yx91.jpg'},
   {name: 'anklyosaurus', image: 'https://www.hoo-zoo.com/wp-content/uploads/2022/10/Ankylosaurus-magniventris-1600x909-1-1200x682.jpeg'},
   {name: 'allosaurus', image: 'https://cdn.mos.cms.futurecdn.net/b5SxqiWwufyfTdgbi9UwHE.jpg'},
   {name: 'spinosaurus', image: 'https://www.dinosaur.org/wp-content/uploads/2024/03/spinosaurus-yelling-above-shallow-water.png'},
